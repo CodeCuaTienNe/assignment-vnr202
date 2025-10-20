dba335baaf8abb916d0ea4455bf624796c023bb5
CodeCuaTienNe <tiendmse182896@fpt.edu.vn>
2025-10-20 03:58:28 +0000

    Deploying to public from @ CodeCuaTienNe/assignment-vnr202@bded9c62268115c8396bc346b4349b86ee7e555b 🚀

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  images: { unoptimized: true },
-  trailingSlash: true,
-  assetPrefix: process.env.BUN_ENV === "production" ? "/Assignment_MLN131" : "",
-};
-
-export default nextConfig;

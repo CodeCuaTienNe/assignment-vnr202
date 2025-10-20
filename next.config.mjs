bded9c62268115c8396bc346b4349b86ee7e555b
Doan Minh Tien <tiendmse182896@fpt.edu.vn>
2025-10-20 10:53:12 +0700

    Update assetPrefix for production environment

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
   output: "export",
   images: { unoptimized: true },
   trailingSlash: true,
-  assetPrefix: process.env.BUN_ENV === "production" ? "/Assignment_MLN131" : "",
+  assetPrefix: process.env.BUN_ENV === "production" ? "/assignment-vnr202" : "",
 };
 
 export default nextConfig;
